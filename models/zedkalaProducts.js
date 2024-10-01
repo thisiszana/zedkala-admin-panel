b066cd38c6baacbfd0ba3c6bebe9a847df53fa37
Zana Amanallhy <zanaamanallhy80@gmail.com>
2024-10-01 12:47:33 +0330

    Writing the products model.

---
+++ models/zedkalaProducts.js
@@ -0,0 +1,74 @@
+import { Schema, models, model } from "mongoose";
+
+const productSchema = new Schema({
+  title: { type: String, required: true },
+  description: { type: String, default: "" },
+  images: { type: [String], required: true },
+  price: { type: Number, required: true },
+  stock: { type: Number, required: true },
+  discount: {
+    value: { type: Number, default: 0 },
+    title: { type: String },
+    expiresAt: { type: Date },
+  },
+  category: { type: Schema.Types.ObjectId, ref: "Category", required: true },
+  subCategory: { type: Schema.Types.ObjectId, ref: "Category" },
+  keywords: { type: [String], default: [] },
+  brand: { type: String, required: true },
+  likes: [{ type: Schema.Types.ObjectId, ref: "Like", default: [] }],
+  comments: [{ type: Schema.Types.ObjectId, ref: "Comment", default: [] }],
+  reviews: [
+    {
+      user: { type: Schema.Types.ObjectId, ref: "User" },
+      rating: { type: Number, min: 0, max: 5, required: true },
+      review: { type: String, required: true },
+      createdAt: { type: Date, default: () => Date.now() },
+    },
+  ],
+  colors: { type: [String], default: ["#ffffff"] },
+  sizes: { type: [String], default: [] },
+  returnPolicy: { type: String },
+  createdAt: {
+    type: Date,
+    default: () => Date.now(),
+    immutable: true,
+  },
+  published: { type: Boolean, default: false },
+  createdBy: { type: Schema.Types.ObjectId, ref: "AdminSorme" },
+  specifications: [
+    {
+      label: { type: String, required: true },
+      value: { type: String, required: true },
+    },
+  ],
+  orders: [
+    {
+      orderId: { type: Schema.Types.ObjectId, ref: "Order" },
+      quantity: { type: Number },
+    },
+  ],
+  warehouses: [
+    {
+      location: { type: String, required: true },
+      stock: { type: Number, required: true },
+    },
+  ],
+  relatedProducts: [{ type: Schema.Types.ObjectId, ref: "ZedkalProducts" }],
+  deliveryOptions: {
+    fastDelivery: { type: Boolean, default: false },
+    freeDelivery: { type: Boolean, default: false },
+  },
+  insurance: {
+    insuranceType: { type: String },
+    insuranceDuration: { type: Number },
+    insuranceCost: { type: Number },
+    insuranceTerms: { type: String },
+    optionalInsurance: { type: Boolean, default: true },
+    mandatoryInsurance: { type: Boolean, default: false },
+  },
+});
+
+productSchema.index({ title: "text", description: "text", brand: "text" });
+
+export const ZedkalProducts =
+  models?.ZedkalProducts || model("ZedkalProducts", productSchema);
