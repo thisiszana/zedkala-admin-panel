262f7d6d7f8bf2c90586a94ce481530d9ae4de4e
Zana Amanallhy <zanaamanallhy80@gmail.com>
2024-11-17 14:25:45 +0330

    Create product detail page action buttons.

--- components/pages/product/ui/ProductAction.jsx
+++ components/pages/product/ui/ProductAction.jsx
@@ -1,7 +1,38 @@
-import React from 'react'
+import Link from "next/link";
 
-export default function ProductAction() {
+import { Tooltip } from "antd";
+
+import { Edit, EyeOpen, LeftAngle } from "@/components/icons/Icons";
+import CustomLink from "@/components/shared/CustomLink";
+import CustomBtn from "@/components/shared/CustomBtn";
+
+export default function ProductAction({ id }) {
   return (
-    <div>ProductAction</div>
-  )
+    <div className="w-full flex justify-between items-center gap-3">
+      <CustomLink
+        href="/products"
+        className="backLink"
+        title="بازگشت"
+        icon={<LeftAngle size={10} />}
+      />
+      <div className="flex items-center gap-3">
+        <Tooltip placement="bottom" title="زنده">
+          <Link href="/" target="_blank">
+            <CustomBtn
+              icon={<EyeOpen className="text-darkGray" />}
+              classNames="iconButton"
+            />
+          </Link>
+        </Tooltip>
+        <Tooltip placement="bottom" title="ویرایش">
+          <Link href={`/products/edit/${id}`}>
+            <CustomBtn
+              icon={<Edit className="text-darkGray" />}
+              classNames="iconButton"
+            />
+          </Link>
+        </Tooltip>
+      </div>
+    </div>
+  );
 }
