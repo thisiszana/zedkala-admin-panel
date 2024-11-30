73d53c699b714e3a795c60e5e9320c3a062ea418
Zana Amanallhy <zanaamanallhy80@gmail.com>
2024-11-30 16:44:17 +0330

    Writing a model related to creating a seller!

---
+++ models/zedkalaVendor.js
@@ -0,0 +1,32 @@
+import { Schema, models, model } from "mongoose";
+
+const zedkalaVendorSchema = new Schema(
+  {
+    firstName: { type: String, required: true },
+    lastName: { type: String, default: "" },
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+    email: { type: String, default: "" },
+    phoneNumber: { type: String, default: "" },
+    address: { type: String, default: "" },
+    country: { type: String, default: "" },
+    avatar: { type: String, default: "" },
+    images: { type: [String], default: "", required: true },
+    taxCode: { type: String, default: "", required: true },
+    businessLicense: { type: String, default: "", required: true },
+    sotreName: { type: String, default: "", required: true },
+    sotreAddress: { type: String, default: "", required: true },
+    productCategory: { type: String, default: "", required: true },
+    roll: { type: String, default: "VENDOR" },
+    terms: { type: Boolean, default: false, required: true },
+    published: { type: Boolean, default: false },
+    productsCreated: [{ type: Schema.Types.ObjectId, ref: "ZedkalaProducts" }],
+  },
+  { timestamps: true }
+);
+
+zedkalaVendorSchema.index({ username: "text", firstName: "text" });
+
+const ZedkalaVendor =
+  models?.ZedkalaVendor || model("ZedkalaVendor", zedkalaVendorSchema);
+export default ZedkalaVendor;
