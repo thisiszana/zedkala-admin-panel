328e3cca3fc3d274c7d9faab5e439e8a51debe22
Zana Amanallhy <zanaamanallhy80@gmail.com>
2024-12-02 00:49:48 +0330

    Create a reusable Skeleton component. And finally, creating a skeleton related to the task list.

--- components/pages/tasks/TasksPage.jsx
+++ components/pages/tasks/TasksPage.jsx
@@ -5,17 +5,17 @@ import Loaderbar from "@/components/shared/Loaderbar";
 import { getServerSession } from "@/utils/session";
 import CreateNewTask from "./ui/CreateNewTask";
 import TasksList from "./ui/TasksList";
+import TasksListSkeletons from "./ui/TasksListSkeletons";
 
-export default function TasksPage() {
+export default function TasksPage({ searchParams }) {
+  const sort = searchParams?.sort || "";
   const session = getServerSession();
 
   return (
     <>
       <PageHeading title="وظایف" />
       <CreateNewTask session={session} />
-      <Suspense fallback={<Loaderbar />}>
-        {" "}
-        {/* این لوادر باید بعدا تغیر کند! */}
+      <Suspense key={sort} fallback={<TasksListSkeletons />}>
         <TasksList />
       </Suspense>
     </>
