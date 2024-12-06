53a1a2f1293e97c57488f8616cf98272b30ecd74
Zana Amanallhy <zanaamanallhy80@gmail.com>
2024-12-06 12:49:09 +0330

    Changing the main skeleton of route tasks.

--- components/pages/tasks/ui/TasksListSkeletons.jsx
+++ components/pages/tasks/ui/TasksListSkeletons.jsx
@@ -2,72 +2,46 @@ import Skeleton from "@/components/shared/Skeleton";
 
 export default function TasksListSkeletons() {
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-8 mt-5">
-      {Array(9)
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 mt-5">
+      {Array(4)
         .fill(null)
-        .map((item, index) => (
+        .map((_, index) => (
           <Skeleton
             key={index}
-            className="rounded-xl w-full p-5 flex flex-col gap-4 ml-3"
+            className="rounded-xl w-full p-5 flex flex-col gap-4 border shadow-sm"
           >
-            <div className="flex items-center justify-between">
-              <div className="flex items-center gap-4">
+            <Skeleton bgColor="bg-gray-300" className="h-4 w-full rounded-lg" />
+            <div className="flex flex-col gap-2">
+              <Skeleton
+                bgColor="bg-gray-300"
+                className="h-6 w-3/4 rounded-lg"
+              />
+              <Skeleton
+                bgColor="bg-gray-300"
+                className="h-4 w-full rounded-lg"
+              />
+            </div>
+            <div className="flex items-center gap-4">
+              <Skeleton
+                bgColor="bg-gray-300"
+                className="h-10 w-10 rounded-full"
+              />
+              <div className="flex flex-col gap-2">
                 <Skeleton
                   bgColor="bg-gray-300"
-                  className="h-10 w-10 rounded-lg"
+                  className="h-4 w-24 rounded-lg"
                 />
                 <Skeleton
                   bgColor="bg-gray-300"
-                  className="h-3 w-20 rounded-lg"
-                />
-              </div>
-              <div className="flex items-center gap-3">
-                <Skeleton
-                  bgColor="bg-gray-300"
-                  className="h-6 w-6 rounded-lg"
-                />
-                <Skeleton
-                  bgColor="bg-gray-300"
-                  className="h-6 w-6 rounded-lg"
+                  className="h-3 w-16 rounded-lg"
                 />
               </div>
             </div>
-            <Skeleton
-              bgColor="bg-gray-300"
-              className="h-6 w-3/4 rounded-full"
-            />
-            <Skeleton
-              bgColor="bg-gray-300"
-              className="h-4 w-full rounded-full"
-            />
-            <Skeleton
-              bgColor="bg-gray-300"
-              className="h-4 w-full rounded-full"
-            />
-            <Skeleton
-              bgColor="bg-gray-300"
-              className="h-4 w-1/2 rounded-full"
-            />
-            <div className="flex items-center justify-between">
-              <div className="flex items-center gap-4">
-                <Skeleton
-                  bgColor="bg-gray-300"
-                  className="h-10 w-10 rounded-full"
-                />
-                <div className="space-y-2">
-                  <Skeleton
-                    bgColor="bg-gray-300"
-                    className="h-2 w-24 rounded-full"
-                  />
-                  <Skeleton
-                    bgColor="bg-gray-300"
-                    className="h-2 w-16 rounded-full"
-                  />
-                </div>
-              </div>
+            <div className="flex justify-between items-center mt-3">
+              <Skeleton bgColor="bg-gray-300" className="h-6 w-16 rounded-lg" />
               <Skeleton
                 bgColor="bg-gray-300"
-                className="h-3 w-16 rounded-full"
+                className="h-6 w-6 rounded-full"
               />
             </div>
           </Skeleton>
