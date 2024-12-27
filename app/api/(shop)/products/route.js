70074aeaedaa11f43a1847d56fe31b5c4418ecd9
Zana Amanallhy <zanaamanallhy80@gmail.com>
2024-12-28 03:09:03 +0330

    set CORS for everyone response

--- app/api/(shop)/products/route.js
+++ app/api/(shop)/products/route.js
@@ -80,6 +80,10 @@ export async function GET(req) {
       },
       { status: STATUS_CODES.success }
     );
+    
+    response.headers.set("Access-Control-Allow-Origin", "*");
+    response.headers.set("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+    response.headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization");
     response.headers.set("Cache-Control", "no-store");
     return response;
   } catch (error) {
